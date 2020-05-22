8c0b7016d7612fef8d063d38ab264fd9f9f74695
Нелли Камалян <62396078+ellkn@users.noreply.github.com>
2020-05-22 18:13:00 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,23 @@ using namespace std;
 int main()
 {
     List<int> a;
-    a.insertLast(1);
-    a.insertFirst(2);
+    a.insertLast(2);
+    a.insertFirst(1);
     a.insertLast(3);
-    a.print() const;
+    a.print();
+    a.deleteAtPos(2); //нумерация с 0, поэтому последння - это 2
+    a.print();
+    a.clear();
+    a.print();
+    a.insertFirst(22);
+    a.insertLast(5);
+    a.insertLast(2020);
+    a.isContains(1);
+    a.print();
+    a.deleteLast();
+    a.getFirst();
+    a.getLast();
+    a.print();
+
     return 0;
 }
